32582afca6df8f5acf64e347f6f211be3e00c37b
Doug Gilliland <doug@douglasgilliland.com>
2014-05-18 09:22:14 -0400

    Added Sphinx comments

--- pyBits.py
+++ pyBits.py
@@ -2,9 +2,19 @@
 '''
 
 class TalkToAPi:
+	'''Simple code to talk to the low level bits of a Raspberry Pi
+	'''
 	def getSwitchValue(self):
+		'''poll the value of a switch
+		
+		:returns: True if the switch is pressed, false if the switch is not pressed
+		'''
 		return True
 	def controlLED(self, ledState):
+		'''turns off or on a LED
+
+		:param ledState: True to turn the light on, false to turn light off
+		'''
 		ledVal = ledState
 		return
 		
