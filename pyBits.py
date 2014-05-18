01fde4f390c07bdde7932216f1946e7c094a1faf
Doug Gilliland <doug@douglasgilliland.com>
2014-05-18 11:11:46 -0400

    Getting more stuff to work...

--- pyBits.py
+++ pyBits.py
@@ -1,9 +1,15 @@
 '''Code to read a switch and light an LED.
 Based on code found at http://makezine.com/projects/tutorial-raspberry-pi-gpio-pins-and-python/
 This code must be run as Superuser on the Raspberry Pi.
+Need to:
+sudo apt-get install python-dev
+sudo apt-get install python-pip python2.7-dev
+sudo apt-get install python-rpi.gpio
 '''
 
 import RPi.GPIO as GPIO
+
+LED_PIN=23
 
 class TalkToAPi:
 	'''Simple code to talk to the low level bits of a Raspberry Pi
@@ -14,10 +20,11 @@ class TalkToAPi:
 		'''
 		GPIO.setmode(GPIO.BOARD)	# setup GPIO using Board numbering
 		GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.setup(23, GPIO.OUT)
+		GPIO.setup(LED_PIN, GPIO.OUT)
 
 	def getSwitchValue(self):
-		'''poll the value of a switch
+		'''Poll the value of a switch/pushbutton.
+		Will probably replace this code later with an interrupt driven routine.
 		
 		:returns: True if the switch is pressed, false if the switch is not pressed
 		'''
@@ -29,11 +36,16 @@ class TalkToAPi:
 			return False
 			
 	def controlLED(self, ledState):
-		'''turns off or on a LED
+		'''turns off or on a LED.
+		The LED is pulled up to Vcc so it needs to be driven low to turn the LED on.
 
 		:param ledState: True to turn the light on, false to turn light off
 		'''
 		ledVal = ledState
+		if ledState == False:
+			GPIO.output(LED_PIN, True)
+		else:
+			GPIO.output(LED_PIN, False)
 		return
 		
 	def getGPIOLibVersion(self):
