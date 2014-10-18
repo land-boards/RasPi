84e12661f0e2455eb2911dcf18b911d7bd1a11ef
Doug Gilliland <doug@douglasgilliland.com>
2014-10-18 12:29:16 -0400

    mo

--- RPI-MDI/ledPulse.py
+++ RPI-MDI/ledPulse.py
@@ -13,8 +13,8 @@ pwm = PWM(0x40, debug=True)
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 while (True):
 	channel = 0
-	duration = 200
-	while duration < 700:
+	duration = 150
+	while duration < 600:
 		pwm.setPWM(channel, 0, duration)
 		time.sleep(.1)
 		duration += 10
