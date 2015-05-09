ff018c4361d920e2785d5793506036fb7d319d45
Doug Gilliland <doug@douglasgilliland.com>
2015-05-09 13:27:30 -0400

    update demo code

--- Adafruit_I2C/Adafruit_MCP230xx.py
+++ Adafruit_I2C/Adafruit_MCP230xx.py
@@ -180,8 +180,8 @@ if __name__ == '__main__':
     # ***************************************************
     # Set num_gpios to 8 for MCP23008 or 16 for MCP23017!
     # ***************************************************
-    mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 8) # MCP23008
-    # mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
+    # mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 8) # MCP23008
+    mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
 
     # Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
     mcp.config(0, mcp.OUTPUT)
