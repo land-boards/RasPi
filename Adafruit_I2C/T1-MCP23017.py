459121dd2430af437fe2961c6ac0aeb1d57795db
Doug Gilliland <doug@douglasgilliland.com>
2015-05-09 13:11:51 -0400

    demo code

---
+++ Adafruit_I2C/T1-MCP23017.py
@@ -0,0 +1,16 @@
+mcp = Adafruit_MCP230XX(busnum = 1, address = 0x27, num_gpios = 16)
+
+# Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
+mcp.config(0, OUTPUT)
+mcp.config(1, OUTPUT)
+mcp.config(2, OUTPUT)
+
+# Set pin 3 to input with the pullup resistor enabled
+# mcp.pullup(3, 1)
+# Read pin 3 and display the results
+# print "%d: %x" % (3, mcp.input(3) >> 3)
+
+# Python speed test on output 0 toggling at max speed
+while (True):
+  mcp.output(0, 1)  # Pin 0 High
+  mcp.output(0, 0)  # Pin 1 Low
