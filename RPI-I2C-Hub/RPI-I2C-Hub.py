4a27b962cd18bcdbab33cd63b70b32cf6362db41
Doug Gilliland <doug@douglasgilliland.com>
2015-10-07 18:27:39 -0400

    added 2nd channel to the test

--- RPI-I2C-Hub/RPI-I2C-Hub.py
+++ RPI-I2C-Hub/RPI-I2C-Hub.py
@@ -13,34 +13,35 @@ GPIOB    = 0x13 # Register for inputs
 
 PCA9544 = 0x71
 SELCH0 = 0X04
-bus.write_byte_data(PCA9544,SELCH0,SELCH0)
+SELCH1 = 0X05
+SELCH2 = 0X06
+SELCH3 = 0X07
 
+bus.write_byte_data(PCA9544,SELCH0,SELCH0)
 # Set all GPA pins as outputs by setting
 # all bits of IODIRA register to 0
 bus.write_byte_data(MCP23017,IODIRA,0x00)
 bus.write_byte_data(MCP23017,IODIRB,0x00)
+# Set output all 16 output bits to 0
+bus.write_byte_data(MCP23017,OLATA,0)
+bus.write_byte_data(MCP23017,OLATB,0)
 
+bus.write_byte_data(PCA9544,SELCH1,SELCH1)
+# Set all GPA pins as outputs by setting
+# all bits of IODIRA register to 0
+bus.write_byte_data(MCP23017,IODIRA,0x00)
+bus.write_byte_data(MCP23017,IODIRB,0x00)
 # Set output all 16 output bits to 0
 bus.write_byte_data(MCP23017,OLATA,0)
 bus.write_byte_data(MCP23017,OLATB,0)
 
 while True:
+	bus.write_byte_data(PCA9544,SELCH0,SELCH0)
 	bus.write_byte_data(MCP23017,OLATA,1)
 	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x02)
+	bus.write_byte_data(MCP23017,OLATA,0)
+
+	bus.write_byte_data(PCA9544,SELCH1,SELCH1)
+	bus.write_byte_data(MCP23017,OLATA,1)
 	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x04)
-	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x08)
-	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x10)
-	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x20)
-	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x40)
-	time.sleep(1)
-	bus.write_byte_data(MCP23017,OLATA,0x80)
-	time.sleep(1)
-  
-# Set all bits to zero
-bus.write_byte_data(MCP23017,OLATA,0)
+	bus.write_byte_data(MCP23017,OLATA,0)
