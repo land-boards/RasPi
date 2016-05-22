36cfd7cab320d5457d33ade2cd68d7d9d96ab582
Doug Gilliland <doug@douglasgilliland.com>
2016-05-22 18:26:32 -0400

    Moved RPPSOC code to RPPSOC repo

--- RPPSOC/HSSP/TimeoutCalc/RegisterDefines.h
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
-* File Name: RegisterDefines.h
-* Version 3.0 plus WiringPi modifications
-* THIS FILE IS NOT REQUIRED SINCE WiringPi DOES NOT NEED DIRECT REGISTER ACCESS.
-*******************************************************************************/
-#ifndef __REGISTERDEFINES_H
-#define __REGISTERDEFINES_H
-    
-#endif /* __REGISTERDEFINES_H */
