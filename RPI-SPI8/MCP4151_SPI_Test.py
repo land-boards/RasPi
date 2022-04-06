3e9185384c630e395e5f52fda300f2311cfba4e2
Doug Gilliland <doug@douglasgilliland.com>
2022-04-06 11:41:56 -0400

    Create MCP4151_SPI_Test.py

---
+++ RPI-SPI8/MCP4151_SPI_Test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# 
+# MCP4151_SPI_Test.py
+# Original code at
+#	https://www.takaitra.com/mcp4151-digital-potentiometer-raspberry-pi/
+# Setup SPI at
+#	https://www.takaitra.com/spi-device-raspberry-pi/
+# 
+# Sample code to write Digital Pot
+# Triangle wave
+# Run with -
+# chmod +x pot-control.py
+# sudo ./pot-control.py
+# 
+
+import spidev
+import time
+
+spi = spidev.SpiDev()
+spi.open(0, 0)		# bus,device
+spi.max_speed_hz = 976000	# speed
+
+def write_pot(input):
+    msb = input >> 8
+    lsb = input & 0xFF
+    spi.xfer([msb, lsb])
+
+while True:
+    for i in range(0x00, 0x1FF, 1):
+        write_pot(i)
+        time.sleep(.005)
+    for i in range(0x1FF, 0x00, -1):
+        write_pot(i)
+        time.sleep(.005)
